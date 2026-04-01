b33968f3e068c1cf868196210cbfea4a39bd6aec
Davender Kumar <davenderkumarlegendboy5.5@gmail.com>
2026-04-01 15:33:41 +0530

    initial commit

---
+++ Cpp Programming/Oop/Q3.cpp
@@ -0,0 +1,40 @@
+/*In C++, you can have more than one constructor in the same class. This is called constructor overloading.
+Each constructor must have a different number or type of parameters, as same as 'function overloading'.*/
+
+//Question: If a driver has submitted his car's documents, then the car's info(like name,model) is
+//          known, otherwise/before that, unknown.
+
+#include<iostream>
+using namespace std;
+
+class Car{
+    public:
+    string carName;
+    string carModel;
+    string ownerName;
+    //Data before registration:
+    Car(){
+        carName = "unknown";
+        carModel = "unknown";
+        ownerName = "unknown";
+    }
+    //Data after registration:
+    Car(string cn,string cm,string on){
+        carName = cn;
+        carModel = cm;
+        ownerName = on;
+    }
+};
+int main(){
+    //Object decleration:
+    Car unknown;
+    Car entry1("BMW","X5","BMW");
+    Car entry2("Volkswagon","Golf","John");
+    
+    //Display Vehicle List:
+    cout<<"Entries without registration: "<<unknown.carName<<" "<<unknown.carModel<<" "<<unknown.ownerName;
+    cout<<"\nEntries with registration: "<<entry1.carName<<" "<<entry1.carModel<<" "<<entry1.ownerName;
+    cout<<"\n"<<entry2.carName<<" "<<entry2.carModel<<" "<<entry2.ownerName;
+    
+    return 0;
+}
