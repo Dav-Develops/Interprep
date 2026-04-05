e65b34d31d297a435a9f75dd4a2ba82745cc9809
Davender Kumar <davenderkumarlegendboy5.5@gmail.com>
2026-04-05 14:54:39 +0530

    initial commit

---
+++ Cpp Programming/Questions/BasicRandom/9ArmstronNumber.cpp
@@ -0,0 +1,38 @@
+// /Write a Program to Check Whether a Number is an Armstrong Number or Not.
+#include<iostream>
+using namespace std;
+
+// Built-in 'pow()' function in '<cmath>' gives wrong answer for some numbers after round off, so i've
+// implemented a custom power function to avoid this issue.
+
+int power(int base, int exponent){
+    int result =1;
+    for (int i=0; i< exponent; i++){
+        result *= base;
+    }
+    return result;
+}
+int armstrong(int n){
+    int original=n,n1 =n;
+    int lastDigit=0;
+    int digits=0;
+    int ans=0;
+
+    while(n!=0){
+        lastDigit=n%10;
+        n/=10;
+        digits++;
+    }
+    while(n1!=0){
+        lastDigit=n1%10;
+        n1/=10;
+        ans = ans+ power(lastDigit,digits);
+        cout<<ans<<"\n";
+    }
+    return ans==original;
+}
+int main(){
+    // cout << armstrong(1634) << endl;
+    armstrong(153)? cout<<"Armstrong Number"<<endl: cout<<"Not an Armstrong Number"<<endl;
+    return 0;
+}
