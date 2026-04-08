0e1acd6f7d373d64e63de381bcd7ae580d2f52a0
Davender Kumar <davenderkumarlegendboy5.5@gmail.com>
2026-04-08 10:47:34 +0530

    initial  commit

--- Cpp Programming/Questions/BasicRandom/21HourglassHorizontal.cpp
+++ Cpp Programming/Questions/BasicRandom/21HourglassHorizontal.cpp
@@ -1,6 +1,5 @@
 // Write a Program to Print the Rotated(Horizontal) Hourglass Pattern.
 
-//----------------------------------Pending Yet------------------------------------------
 #include <iostream>
 using namespace std;
 
@@ -11,32 +10,36 @@ int main()
     cin >> num;
     for (int i = 1; i <= num; i++)
     {
-        for (int j = 1; j < i; j++)
+        for (int j = 1; j <= i; j++)
         {
             // cout << "-";
             cout << "*";
         }
-        for (int k = num * 2 - i; k >= i; k--)
+        for (int k = (num*2) -i; k >= i; k--)
         {
-            cout << "-";
+            // cout << "-";
+            cout << " ";
         }
-        for (int j = 1; j < i; j++)
+        for (int j = 1; j <= i; j++)
         {
-            cout << "-";
+            cout << "*";
         }
         cout << endl;
     }
     for (int i = 1; i <= num; i++)
     {
-        for (int j = num - 1; j >= i; j--)
+        for (int j = num ; j >= i; j--)
         {
             cout << "*";
         }
-        for (int k = 1; k < (i*2) ; k++)
+        for (int k = 1; k <= (i*2)-1 ; k++)
         {
-            cout << "-";
+            // cout << "-";
+            cout << " ";
         }
-        // for(int j=)
+        for(int j= 1; j<(num-i)+2; j++){
+            cout<<"*";
+        }
         cout << endl;
     }
     return 0;
