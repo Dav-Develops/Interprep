807af20522c61ffbaf37c1c681405f7d76eef1c2
Davender Kumar <davenderkumarlegendboy5.5@gmail.com>
2026-04-08 11:54:01 +0530

    Pending Yet

---
+++ Cpp Programming/Questions/BasicRandom/Patterns/25PascalTriangle.cpp
@@ -0,0 +1,34 @@
+//// C++ program to print
+// Pascal’s Triangle
+#include <iostream>
+using namespace std;
+
+//---------------------------Pending Yet---------------------------------
+
+void printPascal(int n)
+{
+
+    int arr[n][n];
+
+    for (int line = 0; line < n; line++) {
+        // Every line has number of integers
+        // equal to line number
+        for (int i = 0; i <= line; i++) {
+            // First and last values in every row are 1
+            if (line == i || i == 0)
+                arr[line][i] = 1;
+            else
+                arr[line][i] = arr[line - 1][i - 1]
+                               + arr[line - 1][i];
+            cout << arr[line][i] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    int n = 10;
+    printPascal(n);
+    return 0;
+}
