bb4c311d8edae086f41845da33f947ea71db226d
Davender Kumar <davenderkumarlegendboy5.5@gmail.com>
2026-04-22 13:38:52 +0530

    initial commit

--- Cpp Programming/Questions/5Data Structures/Stack/BracketSequence1.cpp
+++ Cpp Programming/Questions/5Data Structures/Stack/BracketSequence1.cpp
@@ -1,1 +1,51 @@
 // Code deleted mistakenly..
+// Question here:
+// https://www.w3schools.com/practice/practice.php?problem=WEEKLY002&lang=cpp
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+// Create stack, to store characters:
+stack<char> stk;
+
+bool verify_BracketSequence(string str)
+{
+    for (char ch : str)
+    {
+        if (ch == '(' || ch == '{' || ch == '[')
+        {
+            stk.push(ch);
+        }
+        else
+        {
+            // check is stack is empty or not, cause 'else executed and stack empty means first
+            // bracket is closing bracket, hence the sequence is wrong.
+            if (stk.empty())
+            {
+                return false;
+            }
+
+            char top = stk.top();
+            stk.pop();
+            // If the opening bracket stored in stack does not matches the same type closing one in
+            // string, sequence is incorrect.
+            if ((top == '(' && ch != ')') || (top == '{' && ch != '}') || (top == '[' && ch != ']'))
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    string str;
+    cout << "Enter brackets to evaluate if they are in sequence or not (use only '(',')','{','}','[', and']'): ";
+    getline(cin, str);
+
+    cout<<boolalpha<<verify_BracketSequence(str);
+
+    return 0;
+}
